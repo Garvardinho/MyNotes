d64fb073516c9a038ee8e089ad15c194d9a89ef8
Gavriil Petrov <garvardinho@mail.ru>
2021-11-13 16:06:03 +0300

    Navigation drawer added

--- app/src/main/java/com/garvardinho/mynotes/MainActivity.kt
+++ app/src/main/java/com/garvardinho/mynotes/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_FADE
+import com.google.android.material.navigation.NavigationView
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
@@ -26,17 +31,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        initDrawer(toolbar)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_add) {
+    private fun initDrawer(toolbar: Toolbar) {
+        val drawer: DrawerLayout = findViewById(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            val id: Int = menuItem.itemId
+            if (navigateFragment(id)) {
+                drawer.closeDrawer(GravityCompat.START)
+                return@setNavigationItemSelectedListener true
+            }
+            return@setNavigationItemSelectedListener false
+        }
+    }
+
+    private fun navigateFragment(id: Int): Boolean {
+        if (id == R.id.menu_add) {
             supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.notes, CreateNewNoteFragment())
                 .setTransition(TRANSIT_FRAGMENT_FADE)
                 .addToBackStack(null)
                 .commit()
+
+            return true
         }
+        return false
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (navigateFragment(item.itemId))
+            return true
+
         return super.onOptionsItemSelected(item)
     }
 
